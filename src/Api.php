d290ca14bd5b38fd1882994403f855418e5c75ec
Jasno.Chen <chenjxlg@163.com>
2020-03-04 17:21:25 +0800

    2.0 调整目录结构

--- src/Api.php
+++ src/Api.php
@@ -1,57 +1,13 @@
 <?php
 
-namespace Jason\Api;
+namespace Jason;
 
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Facade;
 
-class Api
+class Api extends Facade
 {
-
-    /**
-     * The version.
-     * @var string
-     */
-    const VERSION = '1.0.0';
-
-    /**
-     * API 认证的看守
-     * @var string
-     */
-    const GUARD = 'api';
-
-    /**
-     * Notes: 获取当前认证用户
-     * @Author: <C.Jason>
-     * @Date: 2020/1/15 1:48 下午
-     * @return mixed
-     */
-    public function user()
+    protected static function getFacadeAccessor()
     {
-        return Auth::guard(self::GUARD)->user();
+        return Api\Api::class;
     }
-
-    /**
-     * Notes: 认证用户
-     * @Author: <C.Jason>
-     * @Date: 2020/1/15 1:51 下午
-     * @param array $credentials
-     * @return mixed
-     */
-    public function attempt(array $credentials)
-    {
-        return Auth::guard(self::GUARD)->attempt($credentials);
-    }
-
-    /**
-     * Notes: 认证用户
-     * @Author: <C.Jason>
-     * @Date: 2020/1/15 1:51 下午
-     * @param $user
-     * @return mixed
-     */
-    public function login($user)
-    {
-        return Auth::guard(self::GUARD)->login($user);
-    }
-
 }
