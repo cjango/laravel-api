b09935cdb293a896d18afd0b35963c11f9282721
C.Jason <chenjxlg@163.com>
2020-05-29 10:21:34 +0800

    config  file

--- src/config/api.php
+++ src/config/api.php
@@ -1,23 +1,40 @@
 <?php
 
 return [
-
+    /**
+     * 认证看守，使用JWT驱动
+     */
     'guard' => [
         'driver'   => 'jwt',
         'provider' => 'users',
         'hash'     => false,
     ],
 
-    'route' => [
-
-        'domain' => env('API_ROUTE_DOMAIN', ''),
-
-        'prefix' => env('API_ROUTE_PREFIX', 'api'),
-
-        'namespace' => 'App\\Api\\Controllers',
-
-        'middleware' => ['api'],
+    'route'     => [
+        /**
+         * 可配置接口独立域名
+         */
+        'domain'          => env('API_ROUTE_DOMAIN', ''),
+        /**
+         * 不实用独立域名，接口地址前缀
+         */
+        'prefix'          => env('API_ROUTE_PREFIX', 'api'),
+        /**
+         * 接口控制器命名空间
+         */
+        'namespace'       => 'App\\Api\\Controllers',
+        /**
+         * 中间件
+         */
+        'middleware'      => ['api'],
+        /**
+         * 身份认证的中间件
+         */
+        'middleware_auth' => ['api', 'token.auth'],
     ],
 
+    /**
+     * 接口目录
+     */
     'directory' => app_path('Api'),
 ];
