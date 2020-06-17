67705b22b474bc22b8ec16a953a9890dc4e44259
C.Jason <chenjxlg@163.com>
2020-06-17 14:13:05 +0800

    部分错误，无法捕获getStatusCode的问题

--- src/Api/Traits/ApiException.php
+++ src/Api/Traits/ApiException.php
@@ -12,11 +12,27 @@ trait ApiException
     {
         return new JsonResponse([
             'status'      => 'EXCEPTION',
-            'status_code' => $e->getStatusCode(),
+            'status_code' => $this->getErrorCode($e),
             'message'     => $e->getMessage(),
             'exception'   => get_class($e),
             'api_uri'     => $request->url(),
         ]);
+    }
+
+    /**
+     * Notes: 有些错误没有 getStatusCode 的问题
+     * @Author: <C.Jason>
+     * @Date  : 2020/6/17 2:11 下午
+     * @param \Throwable $e
+     * @return int
+     */
+    protected function getErrorCode(Throwable $e)
+    {
+        try {
+            return $e->getStatusCode();
+        } catch (Throwable $exception) {
+            return $e->getCode();
+        }
     }
 
 }
