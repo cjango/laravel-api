0195699140d52804d61ef965cf15987edd6d1d7c
C.Jason <chenjxlg@163.com>
2020-06-22 13:38:24 +0800

    api doc

--- src/Api.php
+++ src/Api.php
@@ -3,7 +3,16 @@
 namespace Jason;
 
 use Illuminate\Support\Facades\Facade;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
+/**
+ * Class Api
+ * @package Jason
+ * @method static \Jason\Api\Api attempt(array $credentials)
+ * @method static \Jason\Api\Api login(JWTSubject $user)
+ * @method static \Jason\Api\Api id()
+ * @method static \Jason\Api\Api user()
+ */
 class Api extends Facade
 {
     protected static function getFacadeAccessor()
