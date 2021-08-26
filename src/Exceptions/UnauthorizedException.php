1e317f4ecffd34a5a562ae22eca600dda3e660ac
Jason <chenjxlg@163.com>
2021-08-26 12:15:40 +0800

    增加错误提示

---
+++ src/Exceptions/UnauthorizedException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jason\Api\Exceptions;
+
+use Exception;
+
+class UnauthorizedException extends Exception
+{
+
+    public function __construct()
+    {
+        parent::__construct('Unauthenticated', 401);
+    }
+
+}
--- src/Middleware/Authorized.php
+++ src/Middleware/Authorized.php
@@ -3,17 +3,21 @@
 namespace Jason\Api\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate;
+use Jason\Api\Exceptions\UnauthorizedException;
 
 class Authorized extends Authenticate
 {
 
+    /**
+     * @throws \Jason\Api\Exceptions\UnauthorizedException
+     */
     protected function authenticate($request, array $guards)
     {
         if ($this->auth->guard('api')->check()) {
             return $this->auth->shouldUse('api');
         }
 
-        $this->unauthenticated($request, $guards);
+        throw new UnauthorizedException();
     }
 
 }
