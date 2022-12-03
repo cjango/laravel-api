3a9451faa43e929b440dbeb2b2849ff34fc924e9
小陈叔叔 <chenjxlg@163.com>
2022-12-03 12:21:12 +0800

    Update UnauthorizedException.php

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -8,6 +8,6 @@ class UnauthorizedException extends Exception
 {
     public function __construct()
     {
-        parent::__construct('Unauthenticated.', 401);
+        parent::__construct('未登录或登录凭证已过期.', 401);
     }
 }
